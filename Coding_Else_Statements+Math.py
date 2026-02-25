578f9079057567769806109fec3593b7e147c6c1
Vengeful33 <hunsakerkeaton@gmail.com>
2026-02-25 14:10:41 -0700

    Learning Else, Not, Simple Math and Conditionals

---
+++ Coding_Else_Statements+Math.py
@@ -0,0 +1,27 @@
+#This is how we can use an Else statement:
+
+Summer = False
+
+if Summer:
+    print ("It is Summer")
+else: 
+    print ("It is not Summer")
+
+#An Else statement is a statement that runs when the if statement is false:
+
+#You can also do Math Inside a Blook of code:
+
+High_Score = 10000
+User_Score = 9000
+
+To_High_Score = High_Score - User_Score
+print ("Your where this close to the high Score")
+print (To_High_Score)
+
+CurrentScore = 20  *  50
+print (CurrentScore)
+
+BadScore = 20/50
+print (BadScore)
+
+#You can do the same with + for additon, you can have as many spaces as you want inbetween the variables or numbers, make sure it is on the same line.
---
+++ Using_Conditions.py
@@ -0,0 +1,36 @@
+#Varible Checking, == should be seen as "is equal too sign", we call < > == >= <= != operators:
+Answer = "Four"
+if Answer == "Four":
+    print("Correct")
+
+# != Should be seen as different or not equal too"
+WrongAnswer = "Three"
+if WrongAnswer != "Four":
+    print("Wrong Answer")
+
+#You can use moth greater or equal signs.
+Address = 1001
+if Address >= 900:
+    print("Your close")
+
+#This gets skipped because it is a false statement
+if Address <= 500:
+    print("Your Far Away")
+
+#You can Define variables, boolens, strings and others if they are true or false:
+Day = True
+if Day == True:
+    print("Today is Monday")
+
+#You can use not to change a boolens value
+Night = not True
+if Night == True:
+    print("It is Night")
+
+#Boolens are the type of vaule that is the condition of an if statement:
+
+#Make sure all Boolens are capitalised so no true or false, it has to be True or False:
+
+#Numbers should be without "" so it should be 50 not "50":
+
+#= are not a comparison since it only defines a value.

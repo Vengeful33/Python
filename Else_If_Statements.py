632094b8d9ff04f50958d82cbe579bb32e49edfa
Vengeful33 <hunsakerkeaton@gmail.com>
2026-02-26 14:49:44 -0700

    Else If Statements
    Learned the difference between if, elif and else

---
+++ Else_If_Statements.py
@@ -0,0 +1,16 @@
+
+time = 9
+if time >= 10:
+    print("Noon")
+elif time <= 10:
+    print("Good Morning")
+#elif can be used when there is a second condition, else would be the last "Everything Else" condition
+
+Hour = 10
+if Hour == 12:
+    print("Noon")
+elif Hour == 18:
+    print("Night")
+else:
+    print("Morning")
+#You can use a table like so. You can add more elifs to this statement. 

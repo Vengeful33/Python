96b59d58c4b21e46c4bc1976fd5297987c9335ab
Vengeful33 <hunsakerkeaton@gmail.com>
2026-03-12 19:11:35 -0600

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,19 @@
+
+#We can take a users input with the input() command:
+
+input1 = input()
+print(input1)
+
+#This prints whatever you want down below.
+
+input2 = input("What is your name")
+input3 = input("How old are you?")
+print(input2)
+print(input3)
+
+#This gives you a question on what you should respond too.
+
+input4 = input("Whats your favorite color?")
+print(f"Your favorite color is{input4}")
+
+#This adds a response to the question you asked. 

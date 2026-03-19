976bb0383559e7685b6e785af832a5f499eb00a2
Vengeful33 <hunsakerkeaton@gmail.com>
2026-03-18 18:19:15 -0600

    Updating While Statements

--- While_Statements.py
+++ While_Statements.py
@@ -4,7 +4,16 @@
 while True:
     print ("True")
 
+#While statements only run again once all the code below is ran. If you need to turn off a while statement, turn the variable false.
+
 #If the boolean value is false, the code will not run
 
 while False:
     print("No")
+
+Day = 10
+while Day == 10:
+    print(Day)
+    Day = 12
+
+#Beacause the variable get changed after the loop, it only prints once

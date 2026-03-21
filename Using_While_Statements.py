4e6ce9867511d2e0681da003b501145f733b16cb
Vengeful33 <hunsakerkeaton@gmail.com>
2026-03-20 18:35:55 -0600

    Create Using_While_Statements.py

---
+++ Using_While_Statements.py
@@ -0,0 +1,16 @@
+
+#We can use while statements by giving it a condition it has to follow like:
+
+Time = 12
+while Time <= 24:
+    print(Time)
+    Time += 1
+
+#This prints all numbers until it reaches to 24
+
+Day = 24
+while Day >= 12:
+    print(Day)
+    Day -= 1
+
+#Can be used in reverse as well

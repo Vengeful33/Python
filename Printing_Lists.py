19954ed884fba37c6d7d68c9fdf90b1e137550c9
Vengeful33 <hunsakerkeaton@gmail.com>
2026-04-06 18:57:09 -0600

    Create Printing_Lists.py

---
+++ Printing_Lists.py
@@ -0,0 +1,10 @@
+
+#We can easily print lists using the for loop
+
+Lists = ["CPU", "GPU", "Motherboard", "PSU"]
+
+for list in Lists:
+    print(list)
+    print("-----")
+
+#list could be x, or any other variable we choose, list is just to make it simple

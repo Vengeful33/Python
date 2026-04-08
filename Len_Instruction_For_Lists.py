d68b1f984c398cbf0f1c8e9d675e753cc0a1c337
Vengeful33 <hunsakerkeaton@gmail.com>
2026-04-07 18:53:34 -0600

    Create Len_Instruction_For_Lists.py

---
+++ Len_Instruction_For_Lists.py
@@ -0,0 +1,9 @@
+
+#We can use the len() Instruction to get the number of elements in a list
+
+List = ["Bob", "Mike", "Ann", "Joe"]
+
+LenCommand = len(List)
+print (LenCommand)
+
+#This print 4, LenCommand has a value of 4 after using the len command

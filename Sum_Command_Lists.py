9a10652cfeb191c2dd25bc787d235f483cc08108
Vengeful33 <hunsakerkeaton@gmail.com>
2026-04-10 20:32:02 -0600

    Create Sum_Command_Lists.py

---
+++ Sum_Command_Lists.py
@@ -0,0 +1,9 @@
+
+#We can use the sum() command to help add all the values together from a list:
+
+List = [-2, 6.78, 0, 756]
+Total = sum(List)
+
+print(Total)
+
+#All negatives and floats get added to the total value

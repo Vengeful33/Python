a6c1749633a7f70caf190852b86b93a1afb54860
Vengeful33 <hunsakerkeaton@gmail.com>
2026-04-12 12:49:02 -0600

    Counting and Joining Lists

---
+++ Counting_Lists.py
@@ -0,0 +1,16 @@
+
+#We can use the .count() command to find how many elements are in a list:
+
+Money = [100, 200, 5, 5, 20]
+print(Money.count(5))
+
+#The code on top prints 2, we are also able to save this information in a variable
+
+#Lets say you want to find out how much, but only true or false, then we need to use the in statement
+
+Groceries = ["Bacon", "Eggs", "Milk"]
+has_bacon = "Bacon" in Groceries
+
+print(has_bacon)
+
+#This will then print true, we are also able to print the in loop by itself as well.
---
+++ Joining_Databases.py
@@ -0,0 +1,10 @@
+
+#We can add databases together using the program below:
+
+ListBool = [True, False, True]
+ListNumber = [12, 13, 14]
+
+FullList = ListBool + ListNumber
+print(FullList)
+
+#We can use also strings and floats, the first variable used in a plus list will be in front, and so forth. 

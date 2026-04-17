63524abb072c658fd149dbecdfa8e2e840ddd917
Vengeful33 <hunsakerkeaton@gmail.com>
2026-04-16 21:24:20 -0600

    Naming Functions and Multiple Parameters

---
+++ Functions_With_Names.py
@@ -0,0 +1,14 @@
+
+#We can describe function at a glance by looking at the verb in the function
+
+def create_name(name):
+    return name + " is your name?"
+
+new_name = create_name("Matt")
+print(new_name)
+
+#When using strings try to use create, 
+
+#When using numbers do calculate, get or compute, 
+
+#If booleans like is, has and can.
---
+++ Multiple_Functions.py
@@ -0,0 +1,11 @@
+
+#We can create multiple parameters in the same function:
+
+def get_results(name, last):
+    results = name + " " + last
+    print(results)
+    return results
+
+get_results("Sarah", "Moe")
+
+#We can add as many parameters as we want using commas.
